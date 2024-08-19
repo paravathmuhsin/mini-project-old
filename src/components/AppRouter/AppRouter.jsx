b7314b40e5b4038222b6d9c0def04454397371cf
Muhsin <paravathmuhsin@gmail.com>
2024-08-19 08:59:58 +0530

    initial commit

---
+++ src/components/AppRouter/AppRouter.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Loader from "../Loader/Loader";
+
+const Login = lazy(() => import("../../pages/Login/Login"));
+const Posts = lazy(() => import("../../pages/Post/Posts"));
+const Layout = lazy(() => import("../../components/Layout/Layout"));
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Posts />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+const AppRouter = () => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <RouterProvider router={routes} />
+    </Suspense>
+  );
+};
+
+export default AppRouter;
