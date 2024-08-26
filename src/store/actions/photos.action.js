d055ed942aa380188cacb7bb3821f5f4c406c6a3
akhila <akhilabipin21@gmail.com>
2024-08-26 23:08:46 +0530

    Photos

--- src/store/actions/photos.action.js
+++ src/store/actions/photos.action.js
@@ -4,7 +4,7 @@ import { GET_PHOTOS } from "../types/photo.type";
 const getPhotosAction = (data) => ({ type: GET_PHOTOS, payload: data });
 
 export const fetchPhotos = () => (dispatch) => {
-    getPhotos({first:2}).then((res) => {
+    getPhotos({first:20}).then((res) => {
     dispatch(getPhotosAction(res));
   });
 };
