c8ab42cc4188dd46b977fb1456e127d9d05192c5
Muhsin <paravathmuhsin@gmail.com>
2024-08-27 08:49:14 +0530

    fix

--- src/components/AppContext/AppContext.jsx
+++ src/components/AppContext/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useContext, useState } from "react";
 import PropTypes from "prop-types";
 
 const Context = createContext();
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAppContext = () => useContext(Context);
 const AppContext = ({ children }) => {
   const [appTitle, setAppTitle] = useState("");
